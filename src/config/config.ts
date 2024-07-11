7b3b979416a50c8caae5f5bc4fb78e3f5dc27df7
rifqi <rifqi416@gmail.com>
2024-07-11 10:20:32 +0700

    add env distinction

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,11 +10,14 @@ function getConfig() {
         WEBHOOK_PORT: z.number().default(3000),
         DOMAIN: z.string(),
         MONGO_URL: z.string(),
-        OPENAI_TOKEN: z.string()
+        OPENAI_TOKEN: z.string(),
+        NODE_ENV: z.enum(['dev', 'prod']),
     })
 
     return {
-        ...config
+        ...config,
+        isDev: config.NODE_ENV == 'dev',
+        isProd: config.NODE_ENV == 'prod'
     }
 }
 
